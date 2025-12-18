e0ef61058b5b11863f47e9705e0fde1b07f7bb1a
ierhe <yuningw05@gmail.com>
2025-12-18 12:09:58 -0500

    Update main.js

--- app/src/main.js
+++ app/src/main.js
@@ -1,21 +1,21 @@
 import './style.css'
 
 async function getData(antivirus){
-    try {
-        //go get data
-        const response = await fetch(`https://urlhaus-api.abuse.ch/v1/payloads/recent/`);
-        //handle errors
-        if(response.status != 200){
-            throw new Error(response);
-        }else{
-            //makes the response into json data we can use
-            const data = await response.json();
-            console.log(data)
-            document.getElementById("api-response").textContent = data.name;
-        }
-    } catch (error) {
-        console.log(error);
+  try {
+    //go get data
+    const response = await fetch(`https://urlhaus-api.abuse.ch/v1/payloads/recent/`);
+    //handle errors
+    if(response.status != 200){
+      throw new Error(response);
+    }else{
+      //makes the response into json data we can use
+      const data = await response.json();
+      console.log(data)
+      document.getElementById("api-response").textContent = data.name;
     }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 console.log(getData())
