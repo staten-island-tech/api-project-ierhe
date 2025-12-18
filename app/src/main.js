3eae4f9bc7cb2ae27e3b018be6deeaaa95d737e9
ierhe <yuningw05@gmail.com>
2025-12-18 11:53:55 -0500

    Update main.js

--- app/src/main.js
+++ app/src/main.js
@@ -22,3 +22,23 @@ document.querySelector('#app').innerHTML = `
 `
 
 setupCounter(document.querySelector('#counter'))
+
+async function getData(antivirus){
+    try {
+        //go get data
+        const response = await fetch(`https://urlhaus-api.abuse.ch/v1/payloads/recent/`);
+        //handle errors
+        if(response.status != 200){
+            throw new Error(response);
+        }else{
+            //makes the response into json data we can use
+            const data = await response.json();
+            console.log(data)
+            document.getElementById("api-response").textContent = data.name;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+console.log(getData())
