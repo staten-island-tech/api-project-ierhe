581817546a9e03cfcc5558bb35378654535a32c5
ierhe <yuningw05@gmail.com>
2026-01-05 12:04:00 -0500

    Update main.js

--- app/src/main.js
+++ app/src/main.js
@@ -37,18 +37,41 @@ const next = document.getElementById("nextpage");
 
 let pagenumber = 0
 
-last.addEventListener("click", function () {
+last.addEventListener("click", async function () {
   if (pagenumber == 0) {
     console.log("CANT DO THAT")
   } else{
     pagenumber -= 1
     console.log(pagenumber)
-  }
-});
+    try {
+      const response = await fetch(`https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15`);
+      if(response.status != 200){
+        throw new Error(response)
+      }else{
+        const data = await response.json();
+        data.forEach((title) => console.log(title))
+        return getData;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+}});
 
-next.addEventListener("click", function () {
+next.addEventListener("click", async function () {
   pagenumber++
   console.log(pagenumber)
+  try {
+    const response = await fetch(`https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice="pagenumber"`);
+    if(response.status != 200){
+      throw new Error(response)
+    }else{
+      const data = await response.json();
+      data.forEach((title) => console.log(title))
+      return getData;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 /* https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15&pageNumber=1 */
